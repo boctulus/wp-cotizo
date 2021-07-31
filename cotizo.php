55e284287e9f5b89ce3b4f32da2b331722d3046a
Pablo Bozzolo <boctulus@gmail.com>
2021-07-31 03:16:10 -0300

    .

--- cotizo.php
+++ cotizo.php
@@ -48,13 +48,13 @@ function start_session() {
 function enqueues() 
 {  
 	// JS
-    wp_register_script('prefix_bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js');
-	#wp_register_script('prefix_bootstrap', Files::get_rel_path(). '/assets/js/bootstrap.bundle.min.js');
+    #wp_register_script('prefix_bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js');
+	wp_register_script('prefix_bootstrap', Files::get_rel_path(). '/assets/js/bootstrap.bundle.min.js');
     wp_enqueue_script('prefix_bootstrap');
 
     // CSS
-	#wp_register_style('prefix_bootstrap', Files::get_rel_path() . '/assets/css/bootstrap.min.css');
-    wp_register_style('prefix_bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css');
+	wp_register_style('prefix_bootstrap', Files::get_rel_path() . '/assets/css/bootstrap.min.css');
+    #wp_register_style('prefix_bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css');
     wp_enqueue_style('prefix_bootstrap');
 
 	wp_register_style('prefix_bootstrap', Files::get_rel_path() . '/assets/css/cotizo.css');
@@ -66,6 +66,36 @@ add_action( 'wp_enqueue_scripts', 'enqueues');
 
 // function that runs when shortcode is called
 function wpb_demo_shortcode() {  
+	?>
+
+	<script>
+		document.addEventListener('DOMContentLoaded', () => {
+			let espesor_elem = document.getElementById('thickness');
+			let color_elem = document.getElementById('color');
+		
+			espesor_elem.addEventListener("change", function() {
+				if (espesor_elem.value == 0){
+					espesor_elem.classList.remove('black')
+					espesor_elem.classList.add('grey');
+				} else {
+					espesor_elem.classList.remove('grey')
+					espesor_elem.classList.add('black');
+				}
+			}); 
+
+			color_elem.addEventListener("change", function() {
+				if (color_elem.value == 0){
+					color_elem.classList.remove('black')
+					color_elem.classList.add('grey');
+				} else {
+					color_elem.classList.remove('grey')
+					color_elem.classList.add('black');
+				}
+			}); 
+		}, false);
+	</script>
+	
+	<?php
 	$html = file_get_contents(__DIR__ . '/form.html'); 
 	return $html;
 } 
